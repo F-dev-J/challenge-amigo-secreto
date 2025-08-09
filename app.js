67ee12cc3cde954cbe64a45d0288673182071c0a
Cufa <juarezfacundo39@gmail.com>
2025-08-09 14:48:15 -0300

    se agregaron las funciones 'agregarAmigo' y la funcion de limpiar 'clear'

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaAmigos=[];
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value 
+    if (nombreAmigo=='') {
+       alert("agrege un nombre");
+    } else {
+        listaAmigos.push(nombreAmigo);
+        clear();
+    }
+    console.log(listaAmigos)
+}
+function clear() {
+    let valorCaja = document.querySelector('#amigo');
+    valorCaja.value = "";
+}
